08449999d803c7ff5d83204910969d03d4d645cd
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-05-14 16:26:42 -0300

    Autenticação e Usecases Criados

---
+++ src/Business/Usecases/GetUserInfoUC.ts
@@ -0,0 +1,28 @@
+import{UserDB} from '../../Data/UserDB';
+
+interface GetUserInfoInput {
+    userId: string
+}
+
+interface GetUserInfoOutput {
+    id: string
+    email: string
+}
+
+export class GetUserInfoUC {
+    async execute(input: GetUserInfoInput): Promise<GetUserInfoOutput> {
+           const userDB = new UserDB()
+           
+           const user = await userDB.getUserById(input.userId)
+           
+           if(!user) {
+               throw new Error("Usuário não encontrado")
+           }
+          
+           return {
+            id: user.getId(),
+            email: user.getEmail()
+           }
+    }
+        
+}
